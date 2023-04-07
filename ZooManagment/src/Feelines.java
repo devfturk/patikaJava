c31ece6af64b6e451fadf7460916871c69b101bb
devfturk <furkanturk0@outlook.com.tr>
2023-04-07 12:14:53 +0300

    deneme 2

---
+++ ZooManagment/src/Feelines.java
@@ -0,0 +1,16 @@
+
+public class Feelines extends Animal{
+
+	@Override
+	int getDosage() {
+		// TODO Auto-generated method stub
+		return 20;
+	}
+
+	@Override
+	String getFeedSchedule() {
+		// TODO Auto-generated method stub
+		return "Once a day";
+	}
+
+}
---
+++ ZooManagment/src/Horses.java
@@ -0,0 +1,16 @@
+
+public class Horses extends Animal {
+
+	@Override
+	int getDosage() {
+		// TODO Auto-generated method stub
+		return 10;
+	}
+
+	@Override
+	String getFeedSchedule() {
+		// TODO Auto-generated method stub
+		return "Once a day";
+	}
+
+}
